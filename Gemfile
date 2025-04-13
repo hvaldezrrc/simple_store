5d793fe787c9d454ad72addfee0e3ff6f8146494
hvaldezrrc <hvaldez@rrc.ca>
2025-04-13 16:40:50 -0500

    Added Faker gem and seed script for products

--- Gemfile
+++ Gemfile
@@ -17,6 +17,9 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Add Faker to generate sample data
+gem "faker"
+
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+676.times do
+  Product.create(
+    title: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph,
+    price: Faker::Commerce.price(range: 0.99..99.99),
+    stock_quantity: Faker::Number.between(from: 1, to: 100)
+  )
+end
+
+puts "Created #{Product.count} products"
