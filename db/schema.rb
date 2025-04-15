d770c00d08530b627a4047300720393fbf45b49f
hvaldezrrc <hvaldez@rrc.ca>
2025-04-14 22:53:05 -0500

    Ran migration to create Categories tale

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_13_211348) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_15_035021) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
